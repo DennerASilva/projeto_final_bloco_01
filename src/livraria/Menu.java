230edd407bb2f323c0ed5482324f688122ebf1ae
Denner dos Anjos <denner.a.moura.s@gmail.com>
2024-12-19 10:28:07 -0300

    Método apertarContinuar() implementado na classe Menu

--- src/livraria/Menu.java
+++ src/livraria/Menu.java
@@ -1,5 +1,6 @@
 package livraria;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -11,46 +12,64 @@ public class Menu {
 
 		int opcao;
 
-		System.out.println("|===============================================|");
-		System.out.println("|						|");
-		System.out.println("|		LIVRARIA DOS ANJOS		|");
-		System.out.println("|						|");
-		System.out.println("|===============================================|");
-		System.out.println("|						|");
-		System.out.println("|	1 - Registrar Obra			|");
-		System.out.println("|	2 - Listar Obras no Acervo		|");
-		System.out.println("|	3 - Buscar Obra por código		|");
-		System.out.println("|	4 - Atualizar dados da Obra		|");
-		System.out.println("|	5 - Excluir Obra do acervo		|");
-		System.out.println("|	6 - Sair				|");
-		System.out.println("|						|");
-		System.out.println("|===============================================|");
-		System.out.print("Entre com a opção desejada: ");
+		while (true) {
 
-		try {
-			opcao = ler.nextInt();
-		} catch (InputMismatchException e) {
-			System.out.println("\nDigite valores inteiros.");
-			ler.nextLine();
-			opcao = 0;
-		}
-
-		switch (opcao) {
-
-		case 1 -> System.out.println("\nRegistrar Obra\n");
-		case 2 -> System.out.println("\nListar todas as Obras do Acervo\n");
-		case 3 -> System.out.println("\nBuscar por Código\n");
-		case 4 -> System.out.println("\nAtualizar Dados\n");
-		case 5 -> System.out.println("\nExcluir Obra\n");
-		case 6 -> {
-			System.out.println("\nObrigado por vir!\n");
+			System.out.println("\n|===============================================|");
+			System.out.println("|						|");
+			System.out.println("|		LIVRARIA DOS ANJOS		|");
+			System.out.println("|						|");
 			System.out.println("|===============================================|");
-			System.out.println("|  LIVRARIA DOS ANJOS: Onde ler é o Paraíso	|");
-			sobre();
-			ler.close();
-			System.exit(0);
-		}
-		default -> System.out.println("\nOpção válida.");
+			System.out.println("|						|");
+			System.out.println("|	1 - Registrar Obra			|");
+			System.out.println("|	2 - Listar Obras no Acervo		|");
+			System.out.println("|	3 - Buscar Obra por código		|");
+			System.out.println("|	4 - Atualizar dados da Obra		|");
+			System.out.println("|	5 - Excluir Obra do acervo		|");
+			System.out.println("|	6 - Sair				|");
+			System.out.println("|						|");
+			System.out.println("|===============================================|");
+			System.out.print("Entre com a opção desejada: ");
+
+			try {
+				opcao = ler.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("\nDigite valores inteiros.");
+				ler.nextLine();
+				opcao = 0;
+			}
+
+			switch (opcao) {
+
+			case 1 -> {
+				System.out.println("\nRegistrar Obra\n");
+				apertarContinuar();
+			}
+			case 2 -> {
+				System.out.println("\nListar todas as Obras do Acervo\n");
+				apertarContinuar();
+			}
+			case 3 -> {
+				System.out.println("\nBuscar por Código\n");
+				apertarContinuar();
+			}
+			case 4 -> {
+				System.out.println("\nAtualizar Dados\n");
+				apertarContinuar();
+			}
+			case 5 -> {
+				System.out.println("\nExcluir Obra\n");
+				apertarContinuar();
+			}
+			case 6 -> {
+				System.out.println("\nObrigado por vir!\n");
+				System.out.println("|===============================================|");
+				System.out.println("|  LIVRARIA DOS ANJOS: Onde ler é o Paraíso	|");
+				sobre();
+				ler.close();
+				System.exit(0);
+			}
+			default -> System.out.println("\nOpção inválida. Tente novamente.");
+			}
 		}
 	}
 
@@ -61,5 +80,14 @@ public class Menu {
 		System.out.println("|Denner A M Silva - denner.a.moura.s@gmail.com	|");
 		System.out.println("|github.com/DennerASilva			|");
 		System.out.println("|===============================================|");
+	}
+
+	public static void apertarContinuar() {
+		try {
+			System.out.println("\nPressione Enter para Continuar...");
+			System.in.read();
+		} catch (IOException e) {
+			System.out.println("Você pressionou uma tecla diferentes de Enter!");
+		}
 	}
 }
